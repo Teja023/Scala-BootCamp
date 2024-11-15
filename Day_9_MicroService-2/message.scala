a1fba2bc4e3fffb09db182fcdb5136e9c7631431
Teja Parvathala <tejaparvathala@apples-MacBook-Pro.local>
2024-11-15 22:27:45 +0530

    added microservices

--- Day_9_MicroService-2/message.scala
+++
@@ -1,9 +0,0 @@
-import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
-
-case class Message(messageType : String, message: String, messageKey: String)
-
-object JsonFormat {
-  implicit val messageFormat: RootJsonFormat[Message] = jsonFormat3(Message)
-}
-
